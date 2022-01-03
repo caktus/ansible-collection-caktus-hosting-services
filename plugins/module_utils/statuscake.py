6e23cc957678e9fe4796f4dfbb15c406b98de37d
ronardcaktus <rluna@caktusgroup.com>
2022-01-03 09:54:50 -0500

    5: update test

--- plugins/module_utils/statuscake.py
+++ plugins/module_utils/statuscake.py
@@ -34,9 +34,8 @@ class UptimeTest(StatusCakeAPI):
             logger.debug("All uptime checks in StatusCake: %s", self.response.json()['data'])
             for test in self.response.json()["data"]:
                 if test["name"] == self.data["name"]:
+                    logger.debug(f"Fetched data: {test}")
                     return test
-                    
-
 
     def create(self):
         if "test_type" not in self.data:
@@ -45,14 +44,22 @@ class UptimeTest(StatusCakeAPI):
             self.data["check_rate"] = 300
         self._request("post", self.url, data=self.data)
 
+    def update(self, item_id):
+        if "test_type" not in self.data:
+            self.data["test_type"] = "HTTP"
+        if "check_rate" not in self.data:
+            self.data["check_rate"] = 300
+        self._request("put", f"{self.url}/{item_id}", data=self.data)
 
     def save(self):
         fetch_data = self.fetch()
-        logger.debug(f"{self.data['name']} exists in StatusCake? {bool(fetch_data)}.")
+        logger.debug(f"Does '{self.data['name']}' exists in StatusCake? {bool(fetch_data)}.")
         if not fetch_data:
             self.create()
             logger.info(f"A new test for {self.data['name']} was created.")
-            
+        if fetch_data:
+            self.update(fetch_data["id"])
+            logger.info(f"The test for '{fetch_data['name']}' was updated.")
 
 
 
@@ -74,7 +81,8 @@ if __name__ == '__main__':
     data_loaded = yaml.safe_load(open(parser_file, 'r'))
 
     for uptime_test in data_loaded["uptime_tests"]:
-        if uptime_test['state'] == 'present':
+        if uptime_test['state'] == 'present' and uptime_test['name']:
             logger.info(uptime_test["name"])
+            logger.info(uptime_test["website_url"])
             test = UptimeTest(api_key=data_loaded['api_key'], **uptime_test)
             test.save()
