d39a7be74ac5a020813f43687b5b11dc0df36d0e
ronardcaktus <rluna@caktusgroup.com>
2022-01-03 14:27:57 -0500

    Remove delete

--- plugins/module_utils/statuscake.py
+++ plugins/module_utils/statuscake.py
@@ -51,9 +51,9 @@ class UptimeTest(StatusCakeAPI):
             self.data["check_rate"] = 300
         self._request("put", f"{self.url}/{item_id}", data=self.data)
     
-    def delete(self):
-        fetch_data = self.fetch()
-        self._request("delete", f"{self.url}/{fetch_data['id']}", data=self.data)
+    # def delete(self):
+    #     fetch_data = self.fetch()
+    #     self._request("delete", f"{self.url}/{fetch_data['id']}", data=self.data)
 
     def save(self):
         fetch_data = self.fetch()
@@ -90,5 +90,4 @@ if __name__ == '__main__':
             logger.info(uptime_test["website_url"])
             test = UptimeTest(api_key=data_loaded['api_key'], **uptime_test)
             test.fetch()
-            # test.save()
-            test.delete()
+            test.save()
