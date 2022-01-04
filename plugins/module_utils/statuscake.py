22a979a5c12304e57f149f2f17fb35955d30abb7
ronardcaktus <rluna@caktusgroup.com>
2022-01-04 12:04:19 -0500

    Add delete functionality & make  _create and _update private

--- plugins/module_utils/statuscake.py
+++ plugins/module_utils/statuscake.py
@@ -4,7 +4,7 @@ import yaml
 import sys
 import argparse
 
-logger = logging.getLogger("statuscake")
+logger = logging.getLogger("Statuscake -")
 
 class StatusCakeAPI:
 
@@ -37,33 +37,37 @@ class UptimeTest(StatusCakeAPI):
                     logger.debug(f"Fetched data: {test}")
                     return test
 
-    def create(self):
-        if "test_type" not in self.data:
-            self.data["test_type"] = "HTTP"
-        if "check_rate" not in self.data:
-            self.data["check_rate"] = 300
-        self._request("post", self.url, data=self.data)
+    def _create(self):
+        if self.data['state'] == 'present':
+            if "test_type" not in self.data:
+                self.data["test_type"] = "HTTP"
+            if "check_rate" not in self.data:
+                self.data["check_rate"] = 300
+            self._request("post", self.url, data=self.data)
 
-    def update(self, item_id):
+    def _update(self, item_id):
         if "test_type" not in self.data:
             self.data["test_type"] = "HTTP"
         if "check_rate" not in self.data:
             self.data["check_rate"] = 300
         self._request("put", f"{self.url}/{item_id}", data=self.data)
     
-    # def delete(self):
-    #     fetch_data = self.fetch()
-    #     self._request("delete", f"{self.url}/{fetch_data['id']}", data=self.data)
+    def delete(self):
+        fetch_data = self.fetch()
+        if self.data["state"] == "absent" and self.data["name"] == fetch_data["name"]:
+            self._request("delete", f"{self.url}/{fetch_data['id']}", data=self.data)
+            logger.info(f"The test for '{self.data['name']}' was deleted")
 
     def save(self):
         fetch_data = self.fetch()
         logger.debug(f"Does '{self.data['name']}' exists in StatusCake? {bool(fetch_data)}.")
         if not fetch_data:
-            self.create()
-            logger.info(f"A new test for {self.data['name']} was created.")
-        if fetch_data:
-            self.update(fetch_data["id"])
-            logger.info(f"The test for '{fetch_data['name']}' was updated.")
+            if self.data['state'] == 'present':
+                self._create()
+                logger.debug(f"A new test for '{self.data['name']}' was created.")
+        if fetch_data and self.data["state"] == "present":
+            self._update(fetch_data["id"])
+            logger.info(f"The test for '{fetch_data['name']}' was updated. Uptime: {fetch_data['uptime'] if fetch_data['uptime'] else 'Down' }")
 
 
 
@@ -85,9 +89,10 @@ if __name__ == '__main__':
     data_loaded = yaml.safe_load(open(parser_file, 'r'))
 
     for uptime_test in data_loaded["uptime_tests"]:
-        if uptime_test['state'] == 'present' and uptime_test['name']:
-            logger.info(uptime_test["name"])
-            logger.info(uptime_test["website_url"])
+        if uptime_test["name"]:
             test = UptimeTest(api_key=data_loaded['api_key'], **uptime_test)
-            test.fetch()
-            test.save()
+            # Checks whether tests need to be created or deleted on the UptimeTest
+            if uptime_test["state"] == "present":
+                test.save()
+            else:
+                test.delete()
